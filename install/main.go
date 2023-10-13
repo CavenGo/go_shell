87b71f69740c366cb113c328003c8c0e75f49fd6
luosen <1915227519@qq.com>
2023-10-13 13:52:31 +0800

    新增Linux安装

--- install/main.go
+++ install/main.go
@@ -4,15 +4,25 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"runtime"
 )
 
 func main() {
-
+	os := runtime.GOOS
+	fmt.Println("当前操作系统：", os)
+	if os == "windows" {
+		Windwos()
+	} else if os == "linux" {
+		Linux()
+	} else {
+		fmt.Println("本命令不支持当前当前操作系统")
+	}
+}
+func Windwos() {
 	args := os.Args[1:] // 排除第一个参数，它是可执行文件的路径
 	if len(args) > 0 {
 		// 读取其他参数
 		if len(args) == 1 && args[0] == "?" {
-			fmt.Println("本命令只在windows操作系统下可执行")
 			fmt.Println("运行godinstall不带参数自动安装curl，wget,jq工具")
 			fmt.Println("winget install toolname 使用winget指定安装curl，wget,jq工具")
 			fmt.Println("curl -o (filepath) url 使用curl指定安装go,gcc/g++,cmake")
@@ -41,6 +51,37 @@ func main() {
 			} else {
 				fmt.Println(vaule + " 工具已存在")
 			}
+		}
+	}
+}
+
+func Linux() {
+	args := os.Args[1:] // 排除第一个参数，它是可执行文件的路径
+	if len(args) > 0 {
+		// 读取其他参数
+		if len(args) == 1 && args[0] == "?" {
+			fmt.Println("运行godinstall不带参数自动安装curl，wget,jq工具")
+			fmt.Println("winget install toolname 使用winget指定安装curl，wget,jq工具")
+			fmt.Println("curl -o (filepath) url 使用curl指定安装go,gcc/g++,cmake")
+			return
+		}
+		if len(args) == 2 {
+
+		}
+	} else {
+		cmd := exec.Command("sudo", "apt", "update")
+		cmd.Run()
+		tool := []string{"vim", "jq", "net-tools", "netplan.io", "lshw"}
+		for _, _ = range tool {
+
+		}
+		cmd = exec.Command("bash", "-c", "mkdir /root/source")
+		cmd.Run()
+		cmd = exec.Command("bash", "-c", "which go")
+		goin, _ := cmd.Output()
+		if string(goin) != "" {
+			cmd = exec.Command("bash", "-c", "cp -r ./source/golang /root")
+			cmd.Run()
 		}
 	}
 }
